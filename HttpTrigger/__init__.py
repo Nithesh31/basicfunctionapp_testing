28d4258b2cf7cb449ea8599eb233aa1de2a5c0b7
Kotrike <sai.nithesh.kotrike@accenture.com>
2024-08-27 12:09:47 +0530

    sample functionapp

---
+++ HttpTrigger/__init__.py
@@ -0,0 +1,43 @@
+import logging
+
+import azure.functions as func
+
+a=1;b=2
+
+def addition(a,b):
+    c=a+b
+    return c
+    
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # a = req.params.get('a')
+    # b = req.params.get('a')
+    d = addition(a,b)
+    if d:
+        return func.HttpResponse(f"Hello, a + b = {d}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
+
+#     if name:
+#         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+#     else:
+#         return func.HttpResponse(
+#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+#              status_code=200
+#         )
